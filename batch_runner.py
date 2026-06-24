fbfde99e07f55773ef8eede786ab8a5d311c19fa
Gizem Demir <giizemdmr@example.com>
2026-06-24 13:29:26 +0300

    chore: Guncel Google Drive klasor ID tanimlandi

--- batch_runner.py
+++ batch_runner.py
@@ -13,7 +13,7 @@ except ImportError:
     exit(1)
 
 # --- AYARLAR ---
-DRIVE_FOLDER_ID = "1aCjug9HmzMPLrBZ62be87dKL4K0FIOji"
+DRIVE_FOLDER_ID = "1-jEAyGdrHLFML76-1YXmBe_6CYKayCYK"
 DOWNLOAD_DIR = "downloads"
 OUTPUT_DIR = "ciktilar"
 CONFIG_PATH = "config/pipeline_config.yaml"
